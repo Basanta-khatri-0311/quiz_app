c2a3ca2cbaf8df66fcb0df55e74b7eea25b86937
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-09-01 23:57:54 +0545

    applying button click property to hide the main div

---
+++ script.js
@@ -0,0 +1,6 @@
+const startButton = document.querySelector('.start-btn')
+const startContainer = document.querySelector('.start-container')
+
+startButton.addEventListener('click',()=>{
+    startContainer.style.display = 'none'
+})
