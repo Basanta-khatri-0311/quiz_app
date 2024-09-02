843b6ff1216eaae97dcc9e38b7639d367e981e31
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-09-02 19:45:05 +0545

    added the quiz div

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const startButton = document.querySelector('.start-btn')
 const startContainer = document.querySelector('.start-container')
+ const quizContainer = document.querySelector('.quiz-container')
 
 startButton.addEventListener('click',()=>{
     startContainer.style.display = 'none'
+    quizContainer.style.display = 'block'
 })
 
 const questions = [
